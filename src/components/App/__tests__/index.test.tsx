7acf28e31834e2a34054e5a3856b5da493755353
Petro  Gomes <petrogomes@Camilles-MBP.lan>
2019-12-21 14:24:40 +0000

    Support of absolute paths

--- src/components/App/__tests__/index.test.tsx
+++ src/components/App/__tests__/index.test.tsx
@@ -1,9 +1,12 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "../index";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../index';
 
-test("renders learn react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Given <App />', () => {
+  describe('When initializes', () => {
+    it('Then should match the snapshot', () => {
+      const { container } = render(<App />);
+      expect(container).toMatchSnapshot();
+    });
+  });
 });
